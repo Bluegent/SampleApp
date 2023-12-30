6e8673396af493a2b128e223355bd2b8a89f3b2b
Bluegent <bluegentleman@live.com>
2023-12-30 13:26:29 +0200

    Revert "Introduced a bug"
    This reverts commit a31a9b55a883f6f50752e968280a1b52c7284d06.

--- SampleApp/SampleApp/main.cpp
+++ SampleApp/SampleApp/main.cpp
@@ -4,10 +4,10 @@
 
 
 //let's print the command line arguments
-int main(int argc, const char* argv[])
+int main(int argc, const char * argv[])
 {
     std::cout << "Argument count: " << argc << '\n';
-    for (size_t i = 0; i < argc + 10; ++i)
+    for (size_t i = 0; i < argc; ++i)
     {
         std::cout << "Arg#" << i << ": " << argv[i] << '\n';
     }
