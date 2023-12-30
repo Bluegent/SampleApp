051ba4974eafb55557e4848509e7cc6f9bdf40f5
Bluegent <bluegentleman@live.com>
2023-12-30 13:46:17 +0200

    Added abs function

--- SampleApp/SampleApp/main.cpp
+++ SampleApp/SampleApp/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 
+//write a function that returns the absolute of an integer
+int32_t abs(int32_t num)
+{
+    if (num > 0)
+    {
+        return num;
+    }
+    return -1 * num;
+}
+
+
 //write a function that returns the max of two integers
 int32_t max(int32_t a, int32_t b)
 {
