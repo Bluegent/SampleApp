52bce8e74e9a6bb77ffd81ab82b6c76ccb54c921
Bluegent <bluegentleman@live.com>
2023-12-30 13:24:00 +0200

    Added printing of command line arguments

--- SampleApp/SampleApp/main.cpp
+++ SampleApp/SampleApp/main.cpp
@@ -2,7 +2,14 @@
 
 
 
-int main()
+
+//let's print the command line arguments
+int main(int argc, const char * argv[])
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Argument count: " << argc << '\n';
+    for (size_t i = 0; i < argc; ++i)
+    {
+        std::cout << "Arg#" << i << ": " << argv[i] << '\n';
+    }
+
 }
