f889e47b614cdb601c421372c86ebc8fe4256a43
Bluegent <bluegentleman@live.com>
2023-12-30 15:15:29 +0200

    Refactored main function

--- SampleApp/SampleApp/main.cpp
+++ SampleApp/SampleApp/main.cpp
@@ -20,13 +20,18 @@ int32_t min(int32_t a, int32_t b)
     return a < b ? a : b;
 }
 
-//let's print the command line arguments
-int main(int argc, const char * argv[])
+
+void printArgs(int argc, const char* argv[])
 {
     std::cout << "Argument count: " << argc << '\n';
     for (size_t i = 0; i < argc; ++i)
     {
         std::cout << "Arg#" << i << ": " << argv[i] << '\n';
     }
+}
 
+//let's print the command line arguments
+int main(int argc, const char * argv[])
+{
+    printArgs(argc, argv);
 }
